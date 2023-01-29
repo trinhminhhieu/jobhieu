ecee982914babec996790d1a0731281a2dfba118
network <trinhminhhieuone@gmail.com>
2023-01-29 21:26:29 +0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "../public/assets/css/style.css";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
